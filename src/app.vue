b486281d17dac33931e0af439fe490cc406babfb
Majedul <rumanbsl@gmail.com>
2022-10-14 12:01:32 +0300

    work

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,8 @@
 <script lang="ts" setup>
 const route = useRoute();
-const { data: contentNavs } = await useAsyncData("navigation", () => {
-  return fetchContentNavigation();
-});
+const { data: contentNavs } = await useAsyncData("navigation", () => fetchContentNavigation());
 
-const does = () => {
+const does = computed(() => {
   const title = contentNavs.value.find((content) => content._path === route.path)?.title;
   switch (title) {
     case "Home":
@@ -12,7 +10,7 @@ const does = () => {
     default:
       return "writes";
   }
-};
+});
 const baseUrl = "/";
 </script>
 
@@ -20,7 +18,7 @@ const baseUrl = "/";
   <main>
     <t-container class="navigation">
       <NuxtLink :to="baseUrl">
-        <h1 class="navigation_title">ruman {{ does() }}</h1>
+        <h1 class="navigation_title">ruman {{ does }}</h1>
       </NuxtLink>
       <p class="navigation_blog">
         <NuxtLink :to="baseUrl + 'blog'">Blog</NuxtLink>
@@ -31,8 +29,11 @@ const baseUrl = "/";
 </template>
 
 <style lang="scss">
+/* inject global styles */
 @import "./app.scss";
+</style>
 
+<style lang="scss" scoped>
 .navigation {
   display: flex;
   justify-content: space-between;
--- src/content/index.md
+++ src/content/index.md
@@ -6,7 +6,7 @@ title: Home
   ## About me
 
   I am Majedul Hoque - Fullstack Software Engineer, based in Helsinki, Finland.
-  Currently :t-link{to=https://agilesearch.io/ label=@agile_search external=true} working as a Senior JS engineer.
+  Currently :t-link{to=https://agilesearch.io/ label=@agile_search :external=true} working as a Senior JS engineer.
 
   ### WIP page
 ::
