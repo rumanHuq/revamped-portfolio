958810daa2f74e63abba357cc58b37b3918deb52
Majedul <rumanbsl@gmail.com>
2022-10-14 11:18:27 +0300

    update cmd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ import eslintPlugin from "vite-plugin-eslint";
 export default defineNuxtConfig({
   srcDir: "src/",
   modules: ["@nuxt/content"],
+  target: "static",
+  ssr: false,
   content: {
     highlight: {
       theme: "dark-plus",
@@ -11,5 +13,4 @@ export default defineNuxtConfig({
   vite: {
     plugins: [eslintPlugin()],
   },
-  nitro: { preset: "netlify" },
 });
